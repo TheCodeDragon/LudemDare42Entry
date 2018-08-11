06bbfa0daa67a8c9876a4b629cd1f118b62afa13
TheCodeDragon <TheCodeDragon@users.noreply.github.com>
2018-08-11 13:14:39 +0100

    Bullet and steel
    woot woot

--- Crashbot Arena/Assets/#Final/Scripts/StandardBullet.cs
+++ Crashbot Arena/Assets/#Final/Scripts/StandardBullet.cs
@@ -6,45 +6,35 @@ using UnityEngine;
 public class StandardBullet : MonoBehaviour {
 
     //bullet attributes
-    public float range = 5;
-    public float speed = 5;
-    public float damage = 10;
-
-    public Rigidbody2D rbBullet;
-    public GameObject goBullet;
+    [Header("Bullet Config")]
+    public float range;
+    public float speed;
+    public float damage;
+    //rigidbody
+    Rigidbody2D rbBullet;
 
 	// Use this for initialization
 	void Start () {
 
         //asign rigidbody
         rbBullet = GetComponent<Rigidbody2D>();
-        //assign game object
-        goBullet = GetComponent<GameObject>();
         //add a force to the bullet.
         //Since there is no friction, the bullet will travel at this speed.
-        rbBullet.AddRelativeForce(transform.forward * speed * Time.deltaTime);
+        rbBullet.AddRelativeForce(transform.up * speed);
         //destroy game object after range time
         Destroy(gameObject, range);
 
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-
-    }
-  
-
     // Collision damage and destruction
-    void OnCollisionEnter(Collision col)
+    void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Enemy")
         {
             // Deak danage to foe
             col.gameObject.SendMessage("ApplyDamage", damage);
-        }     
+        }
         Destroy(gameObject);
-
     }
 
 
