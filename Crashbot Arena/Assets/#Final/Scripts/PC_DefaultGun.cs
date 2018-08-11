a520c9a2892f6560777b48665881fa5be3bf3335
Unknown <hatwell.andrew@hotmail.co.uk>
2018-08-11 11:51:52 +0100

    PCDefaultShoot and Basic Bullet

---
+++ Crashbot Arena/Assets/#Final/Scripts/PC_DefaultGun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_DefaultGun : MonoBehaviour {
+
+
+    // Active Weapon
+    public bool activeWeapon = true;
+
+    // Stats for shoosting
+    public float rateOfFire = 2;
+    public bool canShoot = true;
+
+    // Bullet GO
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Run shooting cooldown whenever weapon canShoot = false
+        if (canShoot == false)
+            // check to see if time passed since last shoot is over cooldown
+            if (rateOfFire > 0)
+
+                rateOfFire -= Time.deltaTime;
+
+        // set to can shoot again
+        if (rateOfFire < 0)
+
+            canShoot = true;           
+
+    }
+
+    // PC shoot script
+    void PCShoot () {
+
+        // if this is the active weapon
+        if (activeWeapon == true)
+
+            //on button press
+            if (Input.GetButtonDown("Fire1") && canShoot == true)
+
+                // instantiate bullet
+                Instantiate(bullet, transform.position+(transform.forward*2), transform.rotation);
+                // turn cooldown on
+                canShoot = false;
+               
+
+    }
+
+
+}
---
+++ Crashbot Arena/Assets/#Final/Scripts/StandardBullet.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StandardBullet : MonoBehaviour {
+
+
+    //bullet attributes
+    public float range = 5;
+    public float speed = 5;
+    public float damage = 10;
+
+    public Rigidbody2D rbBullet;
+    public GameObject goBullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rbBullet.velocity = transform.forward * speed;
+
+        float currentAirTime = 0;
+
+        if (currentAirTime < range)
+            currentAirTime += Time.deltaTime;
+
+        if (currentAirTime > range)
+            Destroy(gameObject); 
+
+
+    }
+  
+
+    // Collision damage and destruction
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+            // Deak danage to foe
+            col.gameObject.SendMessage("ApplyDamage", damage);
+
+        Destroy(gameObject);
+
+    }
+
+
+}
