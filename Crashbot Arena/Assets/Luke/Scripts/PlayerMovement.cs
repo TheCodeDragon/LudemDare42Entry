59504dd8af8c2d73450f43d52e9f5f801abbbad4
TheCodeDragon <TheCodeDragon@users.noreply.github.com>
2018-08-11 10:30:11 +0100

    MovementScript
    Moving, looking at camera, maybe even a possible lock-on script!

---
+++ Crashbot Arena/Assets/Luke/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [Header("Movement Config")]//Header tag!
+    public float fl_MoveSpeed;//Movement in absolute, W/S up and down, A/D left and right.
+    public bool bl_CanMove;//In case of stun
+	
+	// Update is called once per frame
+	void Update () {
+        //Check if can move
+        if (bl_CanMove)
+        {
+            //Then move
+            //Depreciated, locks on to mouse location and move relative to it:
+            //MoveRelativeToMouse();
+            //Actual movement
+            MoveInworld();
+            //And rotate
+            LookAtMouse();
+        }
+	}
+    //Movement Scripts
+    void MoveRelativeToMouse()
+    {
+        //Capture the input axies
+        Vector2 V2Movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //apply the transform
+        transform.Translate(V2Movement*fl_MoveSpeed*Time.deltaTime);
+    }
+    void MoveInworld()
+    {
+        //Capture the input axies
+        Vector2 V2Movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //apply the transform to worldspace
+        transform.Translate(V2Movement * fl_MoveSpeed * Time.deltaTime, Space.World);
+    }
+
+
+    void LookAtMouse()
+    {
+        //Get Mouse position on screen
+        Vector3 V3MousePos = Input.mousePosition;
+        //Convert from screen space to world position
+        V3MousePos = Camera.main.ScreenToWorldPoint(V3MousePos);
+        //Set up the direction
+        Vector2 V2Lookdirection = new Vector2(
+            V3MousePos.x - transform.position.x,
+            V3MousePos.y - transform.position.y
+            );
+        //look in that direction
+        transform.up = V2Lookdirection;
+    }
+}
