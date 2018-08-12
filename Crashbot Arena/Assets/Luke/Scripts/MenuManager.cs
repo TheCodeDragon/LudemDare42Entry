e05466d03e7751cd8cac78d754b0bf81af73d513
TheCodeDragon <TheCodeDragon@users.noreply.github.com>
2018-08-12 18:59:07 +0100

    Lift, Menu, More Code
    Lift Sprites, Menu script and Menu!

---
+++ Crashbot Arena/Assets/Luke/Scripts/MenuManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+    [Header("Menu List")]
+    public GameObject[] Menus;
+    public GameObject MainMenu;
+	// Use this for initialization
+	void Start () {
+        //enable the main menu 
+        MainMenu.SetActive(true);
+        //disable the rest
+        foreach (GameObject Menu in Menus)
+        {
+            Menu.SetActive(false);
+        }
+    }
+    public void ChangeMenu(int menuindex)
+    {
+        //disable all Menues.
+        //Disable main menu
+        MainMenu.SetActive(false);
+        //disable rest
+        foreach (GameObject Menu in Menus)
+        {
+            Menu.SetActive(false);
+        }
+        //enable menu
+        Menus[menuindex].SetActive(true);
+
+    }
+    public void ChangeToMainMenu()
+    {
+        //repeat what happened for the start
+        MainMenu.SetActive(true);
+        foreach (GameObject Menu in Menus)
+        {
+            Menu.SetActive(false);
+        }
+    }
+    //exit the game
+    public void ExitGame()
+    {
+        Application.Quit();
+        //Leave debug
+        Debug.Log("I QUIT!");
+    }
+}
