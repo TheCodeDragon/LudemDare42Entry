1e4d8aee69d689f5af13af3753d70ef5597b4ab4
TheCodeDragon <TheCodeDragon@users.noreply.github.com>
2018-08-13 12:09:54 +0100

    Clean Up Script
    Tape it to everything that just has to float around for X time.

---
+++ Crashbot Arena/Assets/Luke/Scripts/CleanUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CleanUp : MonoBehaviour {
+    [Header("CleanUp Time")]
+    [Tooltip("The time, in seconds, till the script cleans it away.")]
+    public float TimeTillCleanup;
+	// Use this for initialization
+	void Start () {
+        //Destroy function.
+        Destroy(gameObject, TimeTillCleanup);
+	}
+}
