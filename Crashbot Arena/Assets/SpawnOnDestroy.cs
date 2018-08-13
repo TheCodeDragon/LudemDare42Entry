be70747dfeb0f2b0ebfcb84bd0f20bb86a8b604c
TheCodeDragon <TheCodeDragon@users.noreply.github.com>
2018-08-13 19:22:35 +0100

    spawnOnDestroy added, audio fix

---
+++ Crashbot Arena/Assets/SpawnOnDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SpawnOnDestroy : MonoBehaviour {
+    //things to spawn, like explosions, etc.
+    public GameObject[] SpawnObjects;
+    //when the gameobject get's destroyed
+    void OnDestroy()
+    {
+        //spawn everything in the array
+        foreach (GameObject SpawnObject in SpawnObjects)
+        {
+            Instantiate(SpawnObject, transform.position, transform.rotation);
+        }
+    }
+}
